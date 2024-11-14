cece3c30075cdb801e7aa1ea65d0bc252f020382
Liudmila Korchikova <liudmila.korchikova@outlook.com>
2024-11-14 12:57:02 +0100

    dynamic path

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -4,12 +4,26 @@ import io.javalin.Javalin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
+
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
+
         app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+        app.get("/hello", ctx ->{
+            String name = ctx.queryParam("name");
+            if (name == null) {
+                name = "World";
+            }
+            ctx.result("Hello " + name + "!");
+        });
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            String userId = ctx.pathParam("id");
+            String postId = ctx.pathParam("postId");
+            ctx.result("User ID: " + userId + ", Post ID: " + postId);
+        });
+
+        app.start(7070);
     }
 }
